7145f827dfb37004ec3667bcf63cdfeaacd7afd5
Key <keynarafaelp@gmail.com>
2025-01-22 15:05:16 -0500

    handle state for invoiceitems

--- tap_stripe/streams.py
+++ tap_stripe/streams.py
@@ -253,6 +253,11 @@ class InvoiceItems(stripeStream):
         # 1. fetch all invoices using rep key
         invoice_item_id = None
         if not self.fetch_from_parent_stream:
+            # invoice_items is a child stream but it also fetches data using its own rep key
+            # so we need to keep the rep_key_value at the header level
+            if "replication_key_value" in self.stream_state:
+                self.stream_state['starting_replication_value'] = self.stream_state['replication_key_value']
+            #---
             invoice_item_id = context.pop("invoice_item_id")
             yield from super().request_records(context)
             self.fetch_from_parent_stream = True
