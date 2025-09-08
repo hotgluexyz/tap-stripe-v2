0fd033fc1cf6b43865643000aa0949c91d4c999b
Mohammad Areeb Siddiqui <mohammadareebs@gmail.com>
2025-09-08 19:45:50 +0500

    hgi-8735: add balance_transactions in DisputesStream (#49)
    * hgi-8735: add balance_transactions in DIsputesStream
    
    * hgi-8735: fix type of balance_transactions
    
    * hgi-8735: remove types.py
    
    * hgi-8735: remove extra whitespaces

--- tap_stripe/streams.py
+++ tap_stripe/streams.py
@@ -1503,6 +1503,29 @@ class DisputesStream(ConcurrentStream):
         th.Property("payment_intent", th.StringType),
         th.Property("reason", th.StringType),
         th.Property("status", th.StringType),
+        th.Property("balance_transactions", th.ArrayType(
+            th.ObjectType(
+                th.Property("id", th.StringType),
+                th.Property("object", th.StringType),
+                th.Property("amount", th.NumberType),
+                th.Property("available_on", th.NumberType),
+                th.Property("created", th.DateTimeType),
+                th.Property("currency", th.StringType),
+                th.Property("description", th.StringType),
+                th.Property("fee", th.NumberType),
+                th.Property("fee_details", th.CustomType({"type": ["array", "string"]})),
+                th.Property("net", th.NumberType),
+                th.Property("reporting_category", th.StringType),
+                th.Property("source", th.StringType),
+                th.Property("status", th.StringType),
+                th.Property("type", th.StringType),
+                th.Property("livemode", th.BooleanType),
+                th.Property("period", th.CustomType({"type": ["object", "string"]})),
+                th.Property("subscription_item", th.StringType),
+                th.Property("total_usage", th.IntegerType),
+                th.Property("exchange_rate", th.NumberType)
+            )
+        )),
     ).to_dict()
 
 
